0b66d09587eae4f499a683ec785c383885feed53
Lucas Cezar <lmcezar@gmail.com>
2023-02-01 22:59:16 -0300

    Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
 import {
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );

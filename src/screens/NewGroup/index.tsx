0eef9178a70561c6e8bee3663906ca3ff5b85f9f
Lucas Cezar <lmcezar@gmail.com>
2023-02-07 10:55:06 -0300

    Criando Componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Hightlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -11,11 +12,15 @@ export function NewGroup() {
 
       <Content>
         <Icon />
+
         <Hightlight
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Button title="Criar" />
+
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );

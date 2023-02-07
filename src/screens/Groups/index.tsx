a48714d7c63e058f461270e6d8aee8dd0e7fb5b2
Lucas Cezar <lmcezar@gmail.com>
2023-02-06 23:50:06 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Hightlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
+
       <Hightlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+
       <GroupCard title="Galera do Ignite" />
     </Container>
   );

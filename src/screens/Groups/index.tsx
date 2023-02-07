52f946017549c8bc97f5cca78198b878e89885da
Lucas Cezar <lmcezar@gmail.com>
2023-02-06 22:58:16 -0300

    Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
+import { Hightlight } from "@components/Highlight";
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Hightlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }

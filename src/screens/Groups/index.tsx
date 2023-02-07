ef4ca7a1543f1eaf0e566ae9b30200d818b9ded5
Lucas Cezar <lmcezar@gmail.com>
2023-02-07 09:51:43 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,10 +4,12 @@ import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Hightlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -19,9 +21,11 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
-
-      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }

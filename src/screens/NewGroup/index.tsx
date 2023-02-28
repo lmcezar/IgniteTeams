c8115c664b51d467ecdefd96a2adae849c8e1103
Lucas Cezar <lmcezar@gmail.com>
2023-02-28 10:12:23 -0300

    Tratando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { AppError } from "@utils/AppError";
+
 import { Container, Content, Icon } from "./styles";
 
 import { groupCreate } from "@storage/group/groupCreate";
@@ -14,10 +17,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possivel criar um novo grupo");
+        console.log(error);
+      }
     }
   }
   return (

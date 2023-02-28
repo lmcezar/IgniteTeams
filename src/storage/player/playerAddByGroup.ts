13e554513fb29a5c2d43771bedd7561ec3459e7e
Lucas Cezar <lmcezar@gmail.com>
2023-02-28 10:39:29 -0300

    Armazenando Players

---
+++ src/storage/player/playerAddByGroup.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppError } from "@utils/AppError";
+
+import { PlayerStorageDTO } from "./PlayerStorageDTO";
+import { PLAYER_COLLECTION } from "@storage/storageConfig";
+
+export async function playerAddByGroup(
+  newPlayer: PlayerStorageDTO,
+  group: string
+) {
+  try {
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, "");
+  } catch (error) {
+    throw error;
+  }
+}
